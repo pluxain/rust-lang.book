8f1278ad7750ad85afb77725c1d04af770a957d6
Pascal Luxain <pascal.luxain@gmail.com>
2024-04-09 10:07:31 +0200

    2-1: handle user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io; // introduce `io` from the standard library as it is not part of the prelude.
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // `let` introduces a variable, immutable by default. `mut` makes the variable mutable
+
+    io::stdin()
+        .read_line(&mut guess) // read line from io into a mutable reference `&mut var`
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }

f8fde5398126bc28b6551c1f3349f55107327472
Pascal Luxain <pascal.luxain@gmail.com>
2024-04-11 13:28:12 +0200

    2-3: guess the random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
+// `cargo doc --open` will also include the documentation of dependencies
+use rand::Rng;
 use std::io; // introduce `io` from the standard library as it is not part of the prelude.
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is {secret_number}");
     println!("Please input your guess.");
 
     let mut guess = String::new(); // `let` introduces a variable, immutable by default. `mut` makes the variable mutable

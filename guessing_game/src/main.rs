f0bd6d94b67765c1e61c305ba1c20317dde039f5
Pascal Luxain <pascal.luxain@gmail.com>
2024-04-15 09:50:09 +0200

    2-6: handle parsing error

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,7 +18,13 @@ fn main() {
             .read_line(&mut guess) // read line from io into a mutable reference `&mut var`
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+        };
 
         println!("You guessed: {guess}");
 

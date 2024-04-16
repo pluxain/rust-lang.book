d0c3f685273a1f312eb750a4e0ec4fafd0ff5ec6
Pascal Luxain <pascal.luxain@gmail.com>
2024-04-16 19:10:53 +0200

    3-3: play with constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,12 @@
+// Constants can be declared in global scope
+const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60;
 fn main() {
+    const UNITS_SECOND: &str = "sec"; // This is not in the book, but I wanted to know how to create a string variable
+    println!("Three hours in seconds is: {THREE_HOURS_IN_SECONDS}{UNITS_SECOND}");
+
+    const SIX_HOURS_IN_SECONDS: u32 = THREE_HOURS_IN_SECONDS * 2;
+    println!("Six hours in seconds is: {SIX_HOURS_IN_SECONDS}{UNITS_SECOND}");
+
     let mut x = 5;
     println!("The value of x is : {x}");
 

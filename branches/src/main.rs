0f6363d55173a14cc8ac9f64063a903763070677
Pascal Luxain <pascal.luxain@gmail.com>
2024-04-17 19:03:41 +0200

    3-15: add first if expressions

---
+++ branches/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let number = 7;
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}

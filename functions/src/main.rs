8cde013d7b9507af919c96633bbee4f0fdff5ede
Pascal Luxain <pascal.luxain@gmail.com>
2024-04-17 18:11:48 +0200

    3-13: use block as expression

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     another_function(5);
 
     print_labeled_measurement(5, 'h');
+
+    blocks_are_expressions();
 }
 
 fn another_function(x: i32) {
@@ -12,4 +14,13 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn blocks_are_expressions() {
+    let y = {
+        let x = 3;
+        x + 1 // !! the missing ; is what make this an expression and not a statement
+    };
+
+    println!("The value of y is: {y}");
 }

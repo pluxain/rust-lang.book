620be999eab3dc14e472900ccef4463d312942f0
Pascal Luxain <pascal.luxain@gmail.com>
2024-05-03 10:41:46 +0200

    4-b: play with return value and ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -17,6 +17,7 @@ fn main() {
     string.push_str(" world!");
     println!("{}", string);
 
+    // Ownership and function
     let ownership_string = String::from("Hello"); // ownership_string comes into scope
     println!("{}", ownership_string);
 
@@ -31,6 +32,21 @@ fn main() {
                    // use x afterward
 
     println!("{}", x); // Works because i32 is Copy
+
+    // Return values and Scope
+    let s1 = gives_ownership(); // gives_ownership moves its return value into s1
+
+    println!("{}", s1);
+
+    let s2 = String::from("hello"); // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2); // s2 is moved into takes_and_gives_back, which also  moves its return value into s3
+
+    println!("{}", s3);
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
 } // Here, x goes out of scope, then ownership_string. But because ownership_string's value was moved, nothing
   // special happens.
 
@@ -44,3 +60,22 @@ fn makes_copy(i: i32) {
     // i comes into scope
     println!("{}", i);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+// gives_ownership will move its
+// return value into the function
+// that calls it
+fn gives_ownership() -> String {
+    let s = String::from("yours"); // s comes into scope
+    s // some_string is returned and moves out to the calling function
+}
+
+// This function takes a String and returns one
+// s comes into scope
+fn takes_and_gives_back(s: String) -> String {
+    s // s is returned and moves out to the calling function
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let l = s.len();
+    (s, l)
+}

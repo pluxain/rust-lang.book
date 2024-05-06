5140306b134186c4e091363767cd4d503e42a806
Pascal Luxain <pascal.luxain@gmail.com>
2024-05-06 21:15:33 +0200

    4-c: init with references and borrowing

---
+++ references-and-borrowing/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+//  refer to some value without taking ownership of it
+// s is a reference to a String
+fn calculate_length(s: &String) -> usize {
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what it refers to, it is not dropped.

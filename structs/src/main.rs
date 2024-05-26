bfee39c91d74221b4ee0808731b42c5cbadb6e12
Pascal Luxain <pascal.luxain@gmail.com>
2024-05-26 19:05:54 +0200

    5-b: use mutable struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,17 +6,25 @@ struct User {
 }
 
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         active: true,
         username: String::from("someusername75"),
         email: String::from("someone@example.com"),
         sign_in_count: 1,
     };
+
+    print_user(&user1);
+    user1.sign_in_count = 2;
+    user1.active = false;
+    print_user(&user1);
+}
+
+fn print_user(u: &User) {
     println!(
         "{}<{}> is {} and signed in {} time(s)",
-        user1.username,
-        user1.email,
-        if user1.active { "active" } else { "not active" },
-        user1.sign_in_count
+        u.username,
+        u.email,
+        if u.active { "active" } else { "not active" },
+        u.sign_in_count
     );
 }

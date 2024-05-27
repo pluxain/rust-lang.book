9302b8042b8ee3768b35c76fe523543357928ad4
Pascal Luxain <pascal.luxain@gmail.com>
2024-05-27 15:16:58 +0200

    5-i: use struct to improve intention

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,11 +1,19 @@
+struct Rectangle {
+    height: u32,
+    width: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.width
 }

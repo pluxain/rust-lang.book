d0cf89f1ea92743dc7bcde36d25bed4ff88d6af5
Pascal Luxain <pascal.luxain@gmail.com>
2024-05-27 16:12:36 +0200

    5-k: use dbg! macro for debugging purpose

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,13 +5,16 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
     println!("rect1 is {:?}", rect1);
     println!("rect1 is {:#?}", rect1);
+
+    dbg!(&rect1);
 
     println!(
         "The area of the rectangle is {} square pixels.",

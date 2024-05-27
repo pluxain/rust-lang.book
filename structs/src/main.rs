7a59fa2b8601da04a2a733ff7d2526d3c2465a02
Pascal Luxain <pascal.luxain@gmail.com>
2024-05-27 07:45:11 +0200

    5-d: create an instance with struct update syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -20,6 +20,18 @@ fn main() {
 
     let user2 = build_user(String::from("test@example.com"), String::from("player one"));
     print_user(&user2);
+
+    // Struct update syntax
+    let user3 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    print_user(&user3);
+    // print_user(&user1); // That would not work because user1 was partially moved in user3
+    user1.username = String::from("Jean-Claude");
+    print_user(&user1); // Now it works and user3 stays as it was
+    print_user(&user3);
 }
 
 fn print_user(u: &User) {

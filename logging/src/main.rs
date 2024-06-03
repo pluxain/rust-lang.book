611f25d2a15ab0429692349d105fbdf62d1b6190
Pascal Luxain <pascal.luxain@gmail.com>
2024-06-03 21:21:12 +0200

    (tech): remove logger lib and setup log4rs in package

--- logging/src/main.rs
+++ logging/src/main.rs
@@ -1,9 +1,11 @@
-use logger;
+use log;
+use log4rs;
 
 fn main() {
+    log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     println!("Hello, world!");
-    logger::debug("Hello world in logger debug mode!");
-    logger::info("Hello world in logger info mode!");
-    logger::warn("Hello world in logger warn mode!");
-    logger::error("Hello world in logger error mode!");
+    log::debug!("Hello world in debug mode!");
+    log::info!("Hello world in info mode!");
+    log::warn!("Hello world in warn mode!");
+    log::error!("Hello world in error mode!");
 }

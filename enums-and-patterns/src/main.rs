a625357c656516dd9d66b89c1063f5d24acfba92
Pascal Luxain <pascal.luxain@gmail.com>
2024-06-04 09:31:34 +0200

    6-g: compare enum and struct

--- enums-and-patterns/src/main.rs
+++ enums-and-patterns/src/main.rs
@@ -7,6 +7,31 @@ use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 //     V4(u8, u8, u8, u8),
 //     V6(String),
 // }
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// Struct equivalents to the one enum
+#[derive(Debug)]
+struct QuitMessage; // unit struct
+
+#[derive(Debug)]
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+struct WriteMessage(String); // tuple struct
+
+#[derive(Debug)]
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Enums");
@@ -18,4 +43,22 @@ fn main() {
 
     log::debug!("{:?}", home);
     log::debug!("{:?}", loopback);
+
+    let e_quit = Message::Quit;
+    let e_move = Message::Move { x: 12, y: 30 };
+    let e_write = Message::Write(String::from("I am moving"));
+    let e_change_color = Message::ChangeColor(120, 120, 120);
+    log::debug!("enum: {:?}", e_quit);
+    log::debug!("enum: {:?}", e_move);
+    log::debug!("enum: {:?}", e_write);
+    log::debug!("enum: {:?}", e_change_color);
+
+    let s_quit = QuitMessage;
+    let s_move = MoveMessage { x: 12, y: 30 };
+    let s_write = WriteMessage(String::from("I am moving"));
+    let s_change_color = ChangeColorMessage(120, 120, 120);
+    log::debug!("struct: {:?}", s_quit);
+    log::debug!("struct: {:?}", s_move);
+    log::debug!("struct: {:?}", s_write);
+    log::debug!("struct: {:?}", s_change_color);
 }

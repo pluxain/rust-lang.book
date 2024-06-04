23bed14e3625c6dca6a3c859161748ec43c3ff98
Pascal Luxain <pascal.luxain@gmail.com>
2024-06-04 09:05:11 +0200

    6-c: debug variables

--- enums-and-patterns/src/main.rs
+++ enums-and-patterns/src/main.rs
@@ -1,19 +1,44 @@
 use log;
 use log4rs;
 
+#[derive(Debug)]
 enum IpAddrKind {
     V4,
     V6,
 }
 
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Enums");
-    let _four = IpAddrKind::V4;
-    let _six = IpAddrKind::V6;
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    log::debug!("{:?}", four);
+    log::debug!("{:?}", six);
 
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    log::debug!("{:?}", home);
+    log::debug!("{:?}", loopback);
 }
 
-fn route(_ip_kind: IpAddrKind) {}
+fn route(ip_kind: IpAddrKind) {
+    log::debug!("route: {:?}", ip_kind);
+}

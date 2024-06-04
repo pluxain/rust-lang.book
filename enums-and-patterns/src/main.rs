f821881981cb182d8be0dae7d9295eea3d18d019
Pascal Luxain <pascal.luxain@gmail.com>
2024-06-04 09:14:13 +0200

    6-e: use different type for enum variants

--- enums-and-patterns/src/main.rs
+++ enums-and-patterns/src/main.rs
@@ -3,14 +3,14 @@ use log4rs;
 
 #[derive(Debug)]
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Enums");
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
 
     log::debug!("{:?}", home);

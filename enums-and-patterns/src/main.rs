c738e172b572327c3f783c3ccd54860dd27b3727
Pascal Luxain <pascal.luxain@gmail.com>
2024-06-04 09:00:58 +0200

    6-b: add logger

--- enums-and-patterns/src/main.rs
+++ enums-and-patterns/src/main.rs
@@ -1,10 +1,14 @@
+use log;
+use log4rs;
+
 enum IpAddrKind {
     V4,
     V6,
 }
 
 fn main() {
-    println!("Enums");
+    log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
+    log::info!("Enums");
     let _four = IpAddrKind::V4;
     let _six = IpAddrKind::V6;
 

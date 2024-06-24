83701f4a8c02f28df12a8937c8c0db8e1f524d54
Pascal Luxain <pascal.luxain@gmail.com>
2024-06-24 16:15:36 +0200

    6-k: add more complex arm code using curly braces

--- enums-and-patterns/src/main.rs
+++ enums-and-patterns/src/main.rs
@@ -131,7 +131,10 @@ fn main() {
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            log::debug!("Lucky penny !");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,

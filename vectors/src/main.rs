9768b57a05aafae4b291a8afcbab17242369017a
Pascal Luxain <pascal.luxain@gmail.com>
2024-08-21 10:36:07 +0200

    8-c: read elements from Vector

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -19,4 +19,24 @@ fn main() {
     v.push(7);
     v.push(8);
     log::info!("Mutated Vector: {:?}", v);
+
+    log::info!("Read Elements of Vectors");
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    log::info!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(found) => log::info!("The third element is {found}"),
+        None => log::info!("There is no third element."),
+    }
+
+    let v = vec![1, 2, 3, 4, 5];
+    // let does_not_exist = &v[100]; // -> panicking
+    // log::info!("Accessing a nonexistent value via index: {does_not_exist}");
+    let does_not_exist = v.get(100);
+    log::info!(
+        "Accessing a nonexistent value via `get`: {:?}",
+        does_not_exist
+    );
 }

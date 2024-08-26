d1c6ab07b7d2111c99f8cb792164db27cb3ffbca
Pascal Luxain <pascal.luxain@gmail.com>
2024-08-26 07:41:18 +0200

    8-l: create Hashmap

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -1,7 +1,13 @@
 use log;
 use log4rs;
+use std::collections::HashMap;
 
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Hashmaps!");
+    log::info!("Creating Hashmaps");
+    let mut scores = HashMap::new();
+    scores.insert(String::from("blue"), 10);
+    scores.insert(String::from("red"), 50);
+    log::info!("{:?}", scores);
 }

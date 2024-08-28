771ffd520b51051089d4e4aae8de32bbfab44a40
Pascal Luxain <pascal.luxain@gmail.com>
2024-08-28 08:45:48 +0200

    challenge: check each input is a number or loop

--- median-and-mode-challenge/src/main.rs
+++ median-and-mode-challenge/src/main.rs
@@ -6,15 +6,29 @@ fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Median and Mode!");
     // @see guessing_game
-    println!("Please enter an integer list separated by spaces:");
-    let mut input = String::new();
-    let mut numbers = Vec::new();
-    stdin()
-        .read_line(&mut input) // read line from io into a mutable reference `&mut var`
-        .expect("Failed to read line");
+    let mut numbers: Vec<i32>;
 
-    for i in input.split_whitespace() {
-        numbers.push(i);
+    'prompt: loop {
+        println!("Please enter a number list separated by spaces:");
+        let mut input = String::new();
+        numbers = vec![];
+        stdin()
+            .read_line(&mut input) // read line from io into a mutable reference `&mut var`
+            .expect("Failed to read line");
+
+        for s in input.split_whitespace() {
+            let number: i32 = match s.parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    log::error!("Wrong input by user {}({})", input, s);
+                    println!("Please enter a number list!");
+                    continue 'prompt;
+                }
+            };
+            numbers.push(number);
+        }
+        break 'prompt;
     }
     log::info!("{numbers:?}");
+    // TODO: sort Vector
 }

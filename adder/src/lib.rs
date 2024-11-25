6b8cf1febba5baccc74fc3de1b5d078d29752906
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-25 16:06:00 +0100

    11-k: add custom failure message

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -7,7 +7,8 @@ pub fn add_two(a: usize) -> usize {
 }
 
 pub fn greeting(name: &str) -> String {
-    format!("Hello {name}!")
+    String::from("Hello!")
+    // format!("Hello {name}!")
 }
 
 #[derive(Debug)]
@@ -72,6 +73,9 @@ mod tests {
     #[test]
     fn greeting_contains_name() {
         let result = greeting("Carol");
-        assert!(result.contains("Carol"));
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"
+        );
     }
 }

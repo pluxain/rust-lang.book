4dc027aa5b36b2d989a94d9ac526ae33cf972585
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-25 16:19:17 +0100

    11-m: refine #[should_panic] to make sure the function panics for the expected reason

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -29,8 +29,10 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {value}.");
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
         }
         Guess { value }
     }
@@ -93,8 +95,14 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(101);
+    }
+
+    #[test]
+    #[should_panic(expected = "greater than or equal to 1")]
+    fn less_than_1() {
+        Guess::new(0);
     }
 }

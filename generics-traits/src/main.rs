549f0a970bb3385ed9a6f5c3afd57a9ccede6bf8
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-28 09:59:12 +0100

    10-c: extract duplicated functional code into a function

--- generics-traits/src/main.rs
+++ generics-traits/src/main.rs
@@ -1,6 +1,17 @@
 use log;
 use log4rs;
 
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Generics and Traits");
@@ -8,24 +19,11 @@ fn main() {
     log::info!("Removing Duplication by Extracting a Function");
 
     let numbers = vec![34, 50, 25, 100, 65];
-    let mut largest = &numbers[0];
+    let result = largest(&numbers);
 
-    for number in &numbers {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    log::info!("The largest number from {:?} is {}", numbers, largest);
+    log::info!("The largest number from {:?} is {}", numbers, result);
 
     let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let mut largest = &numbers[0];
-
-    for number in &numbers {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    log::info!("The largest number from {:?} is {}", numbers, largest);
+    let result = largest(&numbers);
+    log::info!("The largest number from {:?} is {}", numbers, result);
 }

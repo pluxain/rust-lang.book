72f8291cb89cb6ca8d5ec2c020de719f9b0c9445
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-28 09:52:41 +0100

    10-b: find largest number with duplicated code

--- generics-traits/src/main.rs
+++ generics-traits/src/main.rs
@@ -4,4 +4,28 @@ use log4rs;
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Generics and Traits");
+
+    log::info!("Removing Duplication by Extracting a Function");
+
+    let numbers = vec![34, 50, 25, 100, 65];
+    let mut largest = &numbers[0];
+
+    for number in &numbers {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    log::info!("The largest number from {:?} is {}", numbers, largest);
+
+    let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let mut largest = &numbers[0];
+
+    for number in &numbers {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    log::info!("The largest number from {:?} is {}", numbers, largest);
 }

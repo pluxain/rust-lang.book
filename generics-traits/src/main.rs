95ea9a9d0ec939689635db3616a94fb17262bbec
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-28 14:35:12 +0100

    10-e: use Generic to have one function handling compatible types

--- generics-traits/src/main.rs
+++ generics-traits/src/main.rs
@@ -1,22 +1,12 @@
 use log;
 use log4rs;
 
-fn largest_i32(list: &[i32]) -> &i32 {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
         if item > largest {
             largest = item;
-        }
-    }
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
-    let mut largest = &list[0];
-    for item in list {
-        if item > largest {
-            largest = item
         }
     }
     largest
@@ -29,23 +19,23 @@ fn main() {
     log::info!("Removing Duplication by Extracting a Function");
 
     let numbers = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&numbers);
+    let result = largest(&numbers);
 
     log::info!("The largest number from {:?} is {}", numbers, result);
 
     let numbers = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest_i32(&numbers);
+    let result = largest(&numbers);
     log::info!("The largest number from {:?} is {}", numbers, result);
 
     log::info!("Generic Data Types");
     log::info!("In Function Definitions");
 
     let numbers = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&numbers);
+    let result = largest(&numbers);
 
     log::info!("The largest number from {:?} is {}", numbers, result);
 
     let chars = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&chars);
+    let result = largest(&chars);
     log::info!("The largest chars from {:?} is {}", chars, result);
 }

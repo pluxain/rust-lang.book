899e4021bd9b764267e1d4d3e8773d65a003d6b9
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-01 22:43:11 +0100

    11-p: use submodules in Integration Tests

---
+++ adder/tests/common/unreached.rs
@@ -0,0 +1,7 @@
+// Note: this test will not be run as files in subfolders
+// of `tests` don't get compiled as separate crates
+// so are not run
+#[test]
+fn it_is_unreached() {
+    assert!(true);
+}

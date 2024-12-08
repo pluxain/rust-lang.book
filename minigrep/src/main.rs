cdcfc3529e69e549fa46c3debe7716e87afa7661
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-08 17:23:47 +0100

    feat(tech): read from a file and print content
    Ref: #36

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,6 @@
 use log;
 use log4rs;
-use std::env;
+use std::{env, fs};
 
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
@@ -8,5 +8,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let file_path = &args[2];
-    log::info!("Searching for `{query}` in `{file_path}`");
+    log::info!("Searching for `{query}`");
+    log::info!("in `{file_path}`");
+
+    let file_content =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+    log::info!("With text: {file_content}");
 }

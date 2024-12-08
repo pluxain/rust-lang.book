a911f5f454ed89852d50f04f56bf4de23053d85b
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-08 22:17:54 +0100

    feat(tech): extract logic into a function
    reorganize code in file
    
    Ref: #36

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,6 +2,22 @@ use log;
 use log4rs;
 use std::{env, fs, process};
 
+fn main() {
+    log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
+    log::info!("Minigrep: I/O tool");
+
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        log::error!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    log::info!("Searching for `{}`", config.query);
+    log::info!("in `{}`", config.file_path);
+
+    run(config);
+}
+
 struct Config {
     query: String,
     file_path: String,
@@ -18,20 +34,8 @@ impl Config {
     }
 }
 
-fn main() {
-    log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
-    log::info!("Minigrep: I/O tool");
-
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        log::error!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    log::info!("Searching for `{}`", config.query);
-    log::info!("in `{}`", config.file_path);
-
+fn run(config: Config) {
     let file_content =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    log::info!("With text: {file_content}");
+    log::info!("With text:\n{file_content}");
 }

f7e70f1116ef3c98d1413eb22d72d9aed36c208c
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-08 21:46:15 +0100

    feat(tech): refactor function into struct `new` method
    Ref: #36

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,10 +7,12 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config { query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
 
 fn main() {
@@ -18,7 +20,7 @@ fn main() {
     log::info!("Minigrep: I/O tool");
 
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     log::info!("Searching for `{}`", config.query);
     log::info!("in `{}`", config.file_path);

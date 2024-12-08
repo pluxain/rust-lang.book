77047d26a2db8e47e6541a230b9d4adc9066ef88
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-08 17:31:54 +0100

    ref(tech): extract argument parser into a dedicated function
    Ref: #36

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,12 +2,17 @@ use log;
 use log4rs;
 use std::{env, fs};
 
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
+}
+
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Minigrep: I/O tool");
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
     log::info!("Searching for `{query}`");
     log::info!("in `{file_path}`");
 

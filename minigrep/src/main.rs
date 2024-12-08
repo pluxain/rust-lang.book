29796cbe5f12dec63916d4dfe11378e4c902c0b9
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-08 21:41:52 +0100

    feat(tech): use a struct to convey intent
    Ref: #36

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,21 +2,28 @@ use log;
 use log4rs;
 use std::{env, fs};
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
 }
 
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Minigrep: I/O tool");
+
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
-    log::info!("Searching for `{query}`");
-    log::info!("in `{file_path}`");
+    let config = parse_config(&args);
+
+    log::info!("Searching for `{}`", config.query);
+    log::info!("in `{}`", config.file_path);
 
     let file_content =
-        fs::read_to_string(file_path).expect("Should have been able to read the file");
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     log::info!("With text: {file_content}");
 }

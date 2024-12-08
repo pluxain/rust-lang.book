b6f23b669d2326c7c641dc27639d27b1aa660164
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-08 16:58:00 +0100

    feat(tech): read arguments passed to the programm
    Ref: #36

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,10 @@
 use log;
 use log4rs;
+use std::env;
 
 fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Minigrep: I/O tool");
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 }

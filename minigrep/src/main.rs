b1f70fb83b1fbd261a0d0d903f0924f4de4351c4
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-08 17:01:54 +0100

    feat(tech): use variables to save arguments
    Ref: #36

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,5 +6,7 @@ fn main() {
     log4rs::init_file("config/logger.yaml", Default::default()).unwrap();
     log::info!("Minigrep: I/O tool");
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let query = &args[1];
+    let file_path = &args[2];
+    log::info!("Searching for `{query}` in `{file_path}`");
 }
